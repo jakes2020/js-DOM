07e52974f913dd892df36402bfadf35a4651c724
jakes2020 <30989268+jakes2020@users.noreply.github.com>
2017-12-11 11:14:05 -1000

    Add files via upload

---
+++ app.js
@@ -0,0 +1,73 @@
+/*Use the Document Object Model (DOM) to complete the following exercises below:*/
+
+/*1. Taylor Swift
+
+
+Find the element with the id of "name1":
+	- Replace the content with the following string "Tay-Tay"*/
+
+document.getElementById("name1").innerHTML = "Tay-Tay";
+
+/*2. DJ Khaled
+
+Find the element with the id of "position2":
+	- Replace the content with the following string "Project Manager"
+	- Change the background color to red
+*/
+
+document.getElementById("position2").innerHTML = "Project Manager";
+document.getElementById("position2").style.backgroundColor = "red";
+
+/*3. Piko Taro
+
+Find the element with the id of "alias3":
+	- Replace the content with the following string "Concatenation"
+	- Change the font size to 40
+*/
+
+document.getElementById("alias3").innerHTML = "Concatenation";
+document.getElementById("alias3").style.fontSize = "40px";
+
+
+/*4. Prince
+
+Find the element with the class name of "profile": 
+	- Replace the content with a verse from your favorite Prince song
+	- Change the styles of the font-family to Sans Serif
+*/
+
+document.getElementsByClassName("profile").innerHTML = "How can you just leave me standing ? Alone in a world thats so cold? Maybe Im just too demanding Maybe youre just like my mother Shes never satisfied Why do we scream at each other This is what it sounds like When doves cry";
+
+/*5. Bruce Lee
+
+Find the element with the class name of "profile":
+	- Replace the content with a quote from the legend himself
+*/
+
+
+
+/*6. Samuel L Jackson
+
+Find the element with the class name of "alias": 
+	- Replace the content with your favorite character that Sammy portrayed
+*/
+
+
+
+/*7. Peter Griffin
+
+Create a div element and give it an id of "name7":
+	- Inside this div element, give it the contents of "Peter Griffin"
+	- Append this div element to the element with id "nameParent"
+*/
+ 
+/*8. Tim Duncan
+
+Create a div element give it an id of "alias8":
+	- Inside this div element, give it the contents of "Old Man Riverwalk"
+	- Append this div element to the element with id "aliasParent"
+*/
+
+//Final Boss
+/*9. Without modifying the HTML file, create your own profile.*/
+
